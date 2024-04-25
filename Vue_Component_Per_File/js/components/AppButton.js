bc9b91f2d1e09146bb8b9713ba795633a74368c9
Carlos-Baquero <carlosbg14@gmail.com>
2024-04-25 17:00:00 +0200

    estableciendo propiedades con props

--- Vue_Component_Per_File/js/components/AppButton.js
+++ Vue_Component_Per_File/js/components/AppButton.js
@@ -2,12 +2,23 @@ export default {
 
     template: `
       <button 
-      class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled: cursor-not-allowed" 
+      :class="{
+       'border rounded px-5 py-2 disabled:cursor-not-allowed': true, 
+       'bg-blue-200 hover:bg-blue-400': type === 'primary',
+       'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+      }" 
       :disabled="processing"
       >
         <slot />
         </button>
 `,
+
+props: {
+  type: {
+    type: String,
+    default: 'primary'
+  }
+},
 
 data() {
   return {
