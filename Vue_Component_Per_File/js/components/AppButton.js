7f72dcfe60f222b6335c1301b639b2e32fe571a0
Carlos-Baquero <carlosbg14@gmail.com>
2024-04-25 17:27:26 +0200

    reinicio de Vue_Components después de probar los prop components

--- Vue_Component_Per_File/js/components/AppButton.js
+++ Vue_Component_Per_File/js/components/AppButton.js
@@ -4,9 +4,11 @@ export default {
       <button 
       :class="{
        'border rounded px-5 py-2 disabled:cursor-not-allowed': true, 
-       'bg-blue-200 hover:bg-blue-400': type === 'primary',
+       'bg-blue-600 hover:bg-blue-700': type === 'primary',
        'bg-purple-200 hover:bg-purple-400': type === 'secondary',
-      }" 
+       'bg-gray-200 hover:bg-gray-400': type === 'muted',
+       'is-loading': processing
+      }"
       :disabled="processing"
       >
         <slot />
@@ -17,12 +19,13 @@ props: {
   type: {
     type: String,
     default: 'primary'
+  },
+  
+  processing: {
+    type: Boolean,
+    default: false
+    }
   }
-},
+}
+  
 
-data() {
-  return {
-    processing: true
-      };
-    }        
- }
