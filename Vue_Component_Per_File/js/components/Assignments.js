3117cd5933e6b3b54daa18faf378f04bb07e0db4
Carlos-Baquero <carlosbg14@gmail.com>
2024-05-01 19:06:30 +0200

    creando tags para las tareas

--- Vue_Component_Per_File/js/components/Assignments.js
+++ Vue_Component_Per_File/js/components/Assignments.js
@@ -18,9 +18,9 @@ template: `
 data() {
     return {
         assignments: [
-            {name: 'Finish project', complete: false, id: 1},
-            {name: 'Read Chapter 4', complete: false, id: 2},
-            {name: 'Turn in homework', complete: false, id: 3},
+            {name: 'Finish project', complete: false, id: 1, tag: 'math' },
+            {name: 'Read Chapter 4', complete: false, id: 2, tag: 'science' },
+            {name: 'Turn in homework', complete: false, id: 3, tag: 'math'},
         ],
     }
 },
