ebaa8c5c6c2fec5f84c52fdf3eae0182a33a35d6
Carlos-Baquero <carlosbg14@gmail.com>
2024-05-03 13:07:55 +0200

    creando un json para las tareas y anexando un servidor local

--- Vue_Component_Per_File/js/components/Assignments.js
+++ Vue_Component_Per_File/js/components/Assignments.js
@@ -17,11 +17,7 @@ template: `
 
 data() {
     return {
-        assignments: [
-            {name: 'Finish project', complete: false, id: 1, tag: 'math' },
-            {name: 'Read Chapter 4', complete: false, id: 2, tag: 'science' },
-            {name: 'Turn in homework', complete: false, id: 3, tag: 'math'},
-        ],
+        assignments: [],
     }
 },
 
@@ -33,6 +29,14 @@ computed: {
             completed: this.assignments.filter(assignment => assignment.complete),
       };
      }
+    },
+
+    created() {
+        fetch('http://localhost:3001/assignments')
+            .then(response => response.json())
+            .then(assignments => {
+                this.assignments = assignments;
+        });
     },
 
     methods: {
